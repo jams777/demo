f62ebc23c362bf8a70268ce73d4f772e988d7bdd
JiamSeo <jams7777@gmail.com>
2021-03-23 16:12:46 +0900

    auto build test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "hello world. good job\n"
+    return "docker hub auto build \n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
